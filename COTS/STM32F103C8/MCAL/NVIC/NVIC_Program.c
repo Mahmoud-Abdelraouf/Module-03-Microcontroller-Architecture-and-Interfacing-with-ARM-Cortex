a79915238c3d7659e8750c3c3dab5e9a5f2c5408
Mahmoud Abdelraouf Mahmoud <mahmoud126125@gmail.com>
2023-09-08 08:55:59 +0300

    Fix some logical error

--- COTS/STM32F103C8/MCAL/NVIC/NVIC_Program.c
+++ COTS/STM32F103C8/MCAL/NVIC/NVIC_Program.c
@@ -25,7 +25,7 @@ Std_ReturnType MCAL_NVIC_EnableIRQ(IRQn_Type Copy_IRQn)
         NVIC_ISER0 = (1 << Copy_IRQn);
         Local_FunctionStatus = E_OK;
     }
-    else if (Copy_IRQn <= 64)
+    else if (Copy_IRQn < 64)
     {
         NVIC_ISER1 = (1 << (Copy_IRQn - 32));
         Local_FunctionStatus = E_OK;
@@ -53,7 +53,7 @@ Std_ReturnType MCAL_NVIC_DisableIRQ(IRQn_Type Copy_IRQn)
         NVIC_ICER0 = (1 << Copy_IRQn);
         Local_FunctionStatus = E_OK;
     }
-    else if (Copy_IRQn <= 64)
+    else if (Copy_IRQn < 64)
     {
         NVIC_ICER1 = (1 << (Copy_IRQn - 32));
         Local_FunctionStatus = E_OK;
@@ -80,7 +80,7 @@ Std_ReturnType MCAL_NVIC_SetPendingIRQ(IRQn_Type Copy_IRQn)
         NVIC_ISPR0 = (1 << Copy_IRQn);
         Local_FunctionStatus = E_OK;
     }
-    else if (Copy_IRQn <= 64)
+    else if (Copy_IRQn < 64)
     {
         NVIC_ISPR1 = (1 << (Copy_IRQn - 32));
         Local_FunctionStatus = E_OK;
