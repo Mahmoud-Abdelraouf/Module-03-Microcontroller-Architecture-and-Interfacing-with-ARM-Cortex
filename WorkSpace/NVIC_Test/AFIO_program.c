7686f6d5cf41495753d17fc5b3ff6e717b18da6e
Mahmoud Abdelraouf Mahmoud <mahmoud126125@gmail.com>
2023-09-12 20:37:28 +0300

    Update the AFIO

--- WorkSpace/NVIC_Test/AFIO_program.c
+++ WorkSpace/NVIC_Test/AFIO_program.c
@@ -15,41 +15,41 @@
 #include "AFIO_config.h"
 /*****************************< Function Implementations *****************************/
 
-/**<  Function to configure SWJ (Serial Wire JTAG) configuration */
-Std_ReturnType MCAL_AFIO_SetRemap(AFIO_RemapConfig_t remapConfig) 
-{
-    /**< Check if the remapConfig is within valid range */ 
-    if (remapConfig > AFIO_REMAP_CUSTOM)
-    {
-        return E_NOT_OK;  /**< Invalid configuration */ 
-    }
-
-    // Configure the remap settings
-    AFIO->MAPR &= ~AFIO_MAPR_SWJ_CFG_Msk;  /**< Clear SWJ_CFG bits */ 
-
-    switch (remapConfig) 
-    {
-        case AFIO_REMAP_NONE:
-            /**< No remap, SWJ ON without trace (default) */ 
-            break;
-        case AFIO_REMAP_FULL:
-            /**< Full SWJ (JTAG-DP + SW-DP) but without NJTRST */ 
-            AFIO->MAPR |= AFIO_MAPR_SWJ_CFG_001;
-            break;
-        case AFIO_REMAP_PARTIAL:
-            /**< JTAG-DP Disabled and SW-DP Enabled */ 
-            AFIO->MAPR |= AFIO_MAPR_SWJ_CFG_010;
-            break;
-        case AFIO_REMAP_CUSTOM:
-            /**< Custom remap (user-defined) */ 
-            break;
-        default:
-            return E_NOT_OK;  /**< Invalid configuration */ 
-    }
-
-    return E_OK;  /**<  Configuration successful */
-}
-
+///**<  Function to configure SWJ (Serial Wire JTAG) configuration */
+//Std_ReturnType MCAL_AFIO_SetRemap(AFIO_RemapConfig_t remapConfig) 
+//{
+//    /**< Check if the remapConfig is within valid range */ 
+//    if (remapConfig > AFIO_REMAP_CUSTOM)
+//    {
+//        return E_NOT_OK;  /**< Invalid configuration */ 
+//    }
+//
+//    // Configure the remap settings
+//    AFIO->MAPR &= ~AFIO_MAPR_SWJ_CFG_Msk;  /**< Clear SWJ_CFG bits */ 
+//
+//    switch (remapConfig) 
+//    {
+//        case AFIO_REMAP_NONE:
+//            /**< No remap, SWJ ON without trace (default) */ 
+//            break;
+//        case AFIO_REMAP_FULL:
+//            /**< Full SWJ (JTAG-DP + SW-DP) but without NJTRST */ 
+//            AFIO->MAPR |= AFIO_MAPR_SWJ_CFG_001;
+//            break;
+//        case AFIO_REMAP_PARTIAL:
+//            /**< JTAG-DP Disabled and SW-DP Enabled */ 
+//            AFIO->MAPR |= AFIO_MAPR_SWJ_CFG_010;
+//            break;
+//        case AFIO_REMAP_CUSTOM:
+//            /**< Custom remap (user-defined) */ 
+//            break;
+//        default:
+//            return E_NOT_OK;  /**< Invalid configuration */ 
+//    }
+//
+//    return E_OK;  /**<  Configuration successful */
+//}
+//
 /**< Function to configure ADC2_ETRGREG remapping */ 
 void AFIO_MAPR_ADC2ETRGREGRemap(bool Copy_Enable) 
 {
@@ -80,6 +80,9 @@ void AFIO_MAPR_ADC2ETRGINJRemap(bool Copy_Enable)
     AFIO->MAPR = regValue;
 }
 
+
+
+
 Std_ReturnType MCAL_AFIO_SetEXTIConfiguration(u8 Copy_Line, u8 Copy_PortMap)
 {
     Std_ReturnType Local_FunctionStatus = E_NOT_OK;
@@ -96,4 +99,22 @@ Std_ReturnType MCAL_AFIO_SetEXTIConfiguration(u8 Copy_Line, u8 Copy_PortMap)
 
     return Local_FunctionStatus;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 /*****************************< End of Function Implementations *****************************/
--- WorkSpace/NVIC_Test/main.c
+++ WorkSpace/NVIC_Test/main.c
@@ -17,29 +17,30 @@
 /**< APP */
 
 
+
+
 int main(void)
 {
 	/**< init */
 	MCAL_RCC_InitSysClock();
+
+	MCAL_RCC_EnablePeripheral(RCC_APB2, RCC_APB2ENR_IOPAEN);
 	
-	
-	
-	MCAL_NVIC_xSetPriority(35, 10);
-	
-	MCAL_NVIC_SetPendingIRQ(35);
-	
-	
-	
-	MCAL_NVIC_ClearPendingIRQ(35);
+	MCAL_GPIO_SetPinMode(GPIO_PORTA, GPIO_PIN1, GPIO_OUTPUT_PUSH_PULL_2MHZ);
+	MCAL_GPIO_SetPinMode(GPIO_PORTA, GPIO_PIN2, GPIO_OUTPUT_PUSH_PULL_2MHZ);
+	MCAL_GPIO_SetPinMode(GPIO_PORTA, GPIO_PIN3, GPIO_OUTPUT_PUSH_PULL_2MHZ);
 
 	
-	MCAL_NVIC_DisableIRQ(35);
+	MCAL_NVIC_EnableIRQ(6);
+	MCAL_NVIC_EnableIRQ(7);
+		
+	MCAL_NVIC_vSetPriority(7,0,0);
 	
-	MCAL_NVIC_EnableIRQ(36);
+	MCAL_NVIC_vSetPriority(6,2,2);
 	
-	MCAL_NVIC_xSetPriority(35, 8);
-
-
+	MCAL_NVIC_SetPendingIRQ(6);
+	
+	
 	
 	while(1)
 	{
@@ -49,3 +50,19 @@ int main(void)
 		
 	}
 }
+
+
+
+EXTI0_IRQHandler(void)
+{
+	MCAL_GPIO_SetPinValue(GPIO_PORTA, GPIO_PIN1, GPIO_HIGH);
+	MCAL_NVIC_SetPendingIRQ(7);
+	MCAL_GPIO_SetPinValue(GPIO_PORTA, GPIO_PIN2, GPIO_HIGH);
+
+}
+
+EXTI1_IRQHandler(void)
+{
+		MCAL_GPIO_SetPinValue(GPIO_PORTA, GPIO_PIN3, GPIO_HIGH);
+}
+
