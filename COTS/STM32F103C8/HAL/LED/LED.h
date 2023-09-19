509fe4e8d8b0dc1a6ade7ac84b60852a1ebbca4c
Mahmoud Abdelraouf <mahmoud126125#gmail.com>
2023-09-19 10:42:13 +0300

    Update LED Driver

--- COTS/STM32F103C8/HAL/LED/LED.h
+++ COTS/STM32F103C8/HAL/LED/LED.h
@@ -7,76 +7,133 @@
 #ifndef LED_INTERFACE_H_
 #define LED_INTERFACE_H_
 
-typedef u8 LED_Port_t;
-typedef u8 LED_Pin_t;
-
-typedef f32 LED_Delay_ms_t;
+/**
+ * @addtogroup LED_Parameters
+ * @{
+ */
 
 /**
- * @brief Initializes the LED peripheral.
+ * @brief LED Port Enumeration
  *
- * This function initializes the LED peripheral with the specified identifier.
+ * Enumeration of available LED ports.
+ */
+typedef enum {
+    LED_PORTA, /**< Port A */ 
+    LED_PORTB, /**< Port B */
+    LED_PORTC  /**< Port C */
+} LED_Port_t;
+
+/**
+ * @brief LED Pin Enumeration
  *
- * @param[in] LedId The identifier of the LED to initialize.
+ * Enumeration of available LED pins.
+ */
+typedef enum {
+    LED_PIN0,  /**< Pin 0 */ 
+    LED_PIN1,  /**< Pin 1 */
+    LED_PIN2,  /**< Pin 2 */
+    LED_PIN3,  /**< Pin 3 */
+    LED_PIN4,  /**< Pin 4 */
+    LED_PIN5,  /**< Pin 5 */
+    LED_PIN6,  /**< Pin 6 */
+    LED_PIN7,  /**< Pin 7 */
+    LED_PIN8,  /**< Pin 8 */
+    LED_PIN9,  /**< Pin 9 */
+    LED_PIN10, /**< Pin 10 */
+    LED_PIN11, /**< Pin 11 */
+    LED_PIN12, /**< Pin 12 */
+    LED_PIN13, /**< Pin 13 */
+    LED_PIN14, /**< Pin 14 */
+    LED_PIN15, /**< Pin 15 */ 
+} LED_Pin_t;
+
+/**
+ * @brief LED Delay in Milliseconds Type
  *
- * @return None.
+ * Type definition for LED delay in milliseconds.
+ */
+typedef u32 LED_Delay_ms_t;
+
+/**
+ * @} (LED_Parameters)
+ */
+
+
+/**
+ * @defgroup LED_Functions LED Functions
+ * @{
+ */
+
+/**
+ * @brief Initialize an LED
+ *
+ * Initializes the specified LED pin for output.
+ *
+ * @param[in] Copy_LedPortId The ID of the LED port.
+ * @param[in] Copy_LedPinId The ID of the LED pin.
+ * @return Std_ReturnType Returns E_OK if initialization is successful, or E_NOT_OK if an error occurs.
  */
 Std_ReturnType HAL_LED_Init(LED_Port_t Copy_LedPortId, LED_Pin_t Copy_LedPinId);
 
 /**
- * @brief Turns on the specified LED.
+ * @brief Turn On an LED
  *
- * This function turns on the LED with the specified identifier.
+ * Turns on the specified LED.
  *
- * @param[in] LedId The identifier of the LED to turn on.
- *
- * @return None.
+ * @param[in] Copy_LedPortId The ID of the LED port.
+ * @param[in] Copy_LedPinId The ID of the LED pin.
+ * @return Std_ReturnType Returns E_OK if the operation was successful, or E_NOT_OK if an error occurs.
  */
 Std_ReturnType HAL_LED_On(LED_Port_t Copy_LedPortId, LED_Pin_t Copy_LedPinId);
 
 /**
- * @brief Turns off the specified LED.
+ * @brief Turn Off an LED
  *
- * This function turns off the LED with the specified identifier.
+ * Turns off the specified LED.
  *
- * @param[in] LedId The identifier of the LED to turn off.
- *
- * @return None.
+ * @param[in] Copy_LedPortId The ID of the LED port.
+ * @param[in] Copy_LedPinId The ID of the LED pin.
+ * @return Std_ReturnType Returns E_OK if the operation was successful, or E_NOT_OK if an error occurs.
  */
 Std_ReturnType HAL_LED_Off(LED_Port_t Copy_LedPortId, LED_Pin_t Copy_LedPinId);
 
 /**
- * @brief Toggles the specified LED.
+ * @brief Toggle an LED
  *
- * This function toggles the state of the LED with the specified identifier.
+ * Toggles the state of the specified LED.
  *
- * @param[in] LedId The identifier of the LED to toggle.
- *
- * @return None.
+ * @param[in] Copy_LedPortId The ID of the LED port.
+ * @param[in] Copy_LedPinId The ID of the LED pin.
+ * @return Std_ReturnType Returns E_OK if the operation was successful, or E_NOT_OK if an error occurs.
  */
 Std_ReturnType HAL_LED_Toggle(LED_Port_t Copy_LedPortId, LED_Pin_t Copy_LedPinId);
 
 /**
- * @brief Blinks the specified LED once.
+ * @brief Blink an LED Once
  *
- * This function blinks the LED with the specified identifier once.
+ * Blinks the specified LED once with a specified delay.
  *
- * @param[in] LedId The identifier of the LED to blink once.
- *
- * @return None.
+ * @param[in] Copy_LedPortId The ID of the LED port.
+ * @param[in] Copy_LedPinId The ID of the LED pin.
+ * @param[in] Copy_BlinkTime The delay in milliseconds for each blink.
+ * @return Std_ReturnType Returns E_OK if the operation was successful, or E_NOT_OK if an error occurs.
  */
 Std_ReturnType HAL_LED_BlinkOnce(LED_Port_t Copy_LedPortId, LED_Pin_t Copy_LedPinId, LED_Delay_ms_t Copy_BlinkTime);
 
 /**
- * @brief Blinks the specified LED twice.
+ * @brief Blink an LED Twice
  *
- * This function blinks the LED with the specified identifier twice.
+ * Blinks the specified LED twice with a specified delay between blinks.
  *
- * @param[in] LedId The identifier of the LED to blink twice.
- *
- * @return None.
+ * @param[in] Copy_LedPortId The ID of the LED port.
+ * @param[in] Copy_LedPinId The ID of the LED pin.
+ * @param[in] Copy_BlinkTime The delay in milliseconds for each blink.
+ * @return Std_ReturnType Returns E_OK if the operation was successful, or E_NOT_OK if an error occurs.
  */
 Std_ReturnType HAL_LED_BlinkTwice(LED_Port_t Copy_LedPortId, LED_Pin_t Copy_LedPinId, LED_Delay_ms_t Copy_BlinkTime);
 
+/**
+ * @} (end of LED_Functions)
+ */
 
 #endif /**< LED_INTERFACE_H_ */
